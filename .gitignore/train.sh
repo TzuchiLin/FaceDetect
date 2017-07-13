abec3a0876ff5984a4f385a183cabf7d806dfa22
TzuchiLin <kaylin7938@gmail.com>
2017-07-13 15:26:34 +0800

    Delete train.sh

--- .gitignore/train.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-/home/kay/caffe/build/tools/caffe train --solver=/home/kay/Project/FaceDetect/solver.prototxt #\
-#--weights=models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
-#--gpu 0 
