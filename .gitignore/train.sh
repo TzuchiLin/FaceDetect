e91710fa9aaba86c22f332e69627987dfa49ab66
TzuchiLin <kaylin7938@gmail.com>
2017-07-13 15:18:02 +0800

    Add files via upload

---
+++ .gitignore/train.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+/home/kay/caffe/build/tools/caffe train --solver=/home/kay/Project/FaceDetect/solver.prototxt #\
+#--weights=models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
+#--gpu 0 
